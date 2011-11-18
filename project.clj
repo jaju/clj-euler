67cf6ce078f56b70bf7ea0795aa50c39fdec594c
Ravindra R. Jaju <jaju@msync.org>
2011-11-18 20:22:10 +0530

    Updated to clojure 1.3.0 and swank-clojure to 1.3.3

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clj-euler "0.1.0-SNAPSHOT"
   :description "My attempt at the Euler problems in clojure"
   :dev-dependencies [[lein-diagnostics "0.0.1"] [marginalia "0.5.1"]]
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [swank-clojure "1.3.1"]
+                 [swank-clojure "1.3.3"]
                  ]
   :tasks [marginalia.tasks]
    )
